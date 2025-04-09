2f3960234dcd4b799e7aab15d080b98b6fe89a72
vicmarp <vicmarp2@gmail.com>
2025-04-09 19:54:14 +0300

    "LB-02: First variant of the code"

---
+++ Padilla_LR3_project.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+#include <conio.h>
+using namespace std;
+
+void askFirstNumber(int& num1);
+void askSecondNumber(int& num2);
+string addRight(int& num1,int& num2);
+string addLefth(int& num1,int& num2);
+int menu();
+
+int main(){
+    menu();
+    system("pause");
+    return 0;
+}
+
+int menu(){
+    //Developed by Victor Padilla - branch branch_fun_1
+}
+void askFirstNumber(int& num1){
+    //Developed by Victor Padilla - branch branch_fun_2
+}
+void askSecondNumber(int& num2){
+    //Developed by Victor Padilla - branch branch_fun_3
+}
+string addRight(int& num1,int& num2){
+    //Developed by Victor Padilla - branch branch_fun_4
+}
+string addLefth(int& num1,int& num2){
+    //Developed by Victor Padilla - branch branch_fun_5
+}
